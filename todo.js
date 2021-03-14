3bb76e2950e34c221451c2f54f2e6e36f1f6700c
Henry Kolawole <henrykay53@gmail.com>
2021-03-14 13:20:10 +0100

    js changed

--- todo.js
+++ todo.js
@@ -1,54 +1,21 @@
-// creating close button and appending it to each list item
-var myNodeList = $('li');
-for (var i = 0; i < myNodeList.length; i++) {
-  var span = document.createElement('span');
-  var txt = document.createTextNode("u\00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodeList[i].appendChild(span);
+$(function() {
 
-};
+  //declear variables
+  var listItem = $('input:text');
+  var addButton = $('#addBtn');
 
-//click on close button to hide list item
-var close = $(".close");
-for (var i=0; i < close.length; i++) {
-  close[i].on('click', function() {
-    var div = this.parentElement;
-    div.style.display = "none";
+  addButton.on('click', function() {
+
+    var text = $('input:text').val();
+
+    if (text === ' ') {
+      $(alert('Name can not be blank'));
+    } else {
+      $('#my--list').append('<li>' + text + '</li>'); // add list item to list
+    }
+    listItem.val(' ');
   });
-}
 
-// Create a new list item when clicking on the "Add" button
-function newElement() {
-
-  var li = document.createElement("li");
-  var inputValue = $("#list--item").val();
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-
-  if (inputValue === '') {
-    alert("You must write something!");
-  }
-  else {
-  $("#my--list").appendChild(li);
-  }
-
-  $("#list--item").val('');
-  
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-  
-  for (i = 0; i < close.length; i++) {
-    close[i].on('click', function() {
-      var div = this.parentElement;
-      div.style.display= "none";
-    })
-  }
-}
- 
-console.log('Hello');
+});
 
 
